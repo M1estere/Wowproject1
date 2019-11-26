f93159f9b8c210fe59b211ff8bfc276723f4d0d1
M1estere <7156643@gmail.com>
2019-11-26 15:35:19 +0300

    Angular

--- wowproject/src/app/app.component.ts
+++ wowproject/src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+
+class Item{
+  purchase: string;
+  done: boolean;
+  price: number;
+   
+  constructor(purchase: string, price: number) {
+
+      this.purchase = purchase;
+      this.price = price;
+      this.done = false;
+  }
+}
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +23,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wowproject';
+
+  text:string;
+  price:number;
+
+    items: Item[] = [
+        { purchase: "Хлеб", done: false, price: 15.9 },
+        { purchase: "Масло", done: false, price: 60 },
+        { purchase: "Картофель", done: true, price: 22.6 },
+        { purchase: "Сыр", done: false, price:310 }
+    ];
+    addItem(): void {
+
+        if(this.text==null || this.text.trim()=="" || this.price==null)
+            return;
+        this.items.push(new Item(this.text, this.price));
+    }
 }
+
