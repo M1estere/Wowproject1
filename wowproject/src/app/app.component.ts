674d74602bed3d33f1f69641384632ea57c5c95e
M1estere <1234515@MAIL.RU>
2019-12-03 15:54:06 +0300

    Леха тупил

--- wowproject/src/app/app.component.ts
+++ wowproject/src/app/app.component.ts
@@ -7,15 +7,7 @@ import { FormsModule } from '@angular/forms'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   clicks: number = 0;
-   onChanged(increased) {
-     if (increased) {
-       this.clicks = this.clicks + 1;
-     } 
-     else {
-       this.clicks = this.clicks - 1;
-     }
-   }
+name: string = "Tom";
 }
 
 
