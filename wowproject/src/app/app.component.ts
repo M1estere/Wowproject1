efeebb18a3274a6ad36a1e035af874d237da3014
M1estere <1234515@MAIL.RU>
2019-12-10 15:01:57 +0300

    Главная кнопка

--- wowproject/src/app/app.component.ts
+++ wowproject/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,12 @@ import { FormsModule } from '@angular/forms'
 })
 export class AppComponent {
 name: string = "Tom";
+
+constructor(private router: Router) {}
+goHome() {
+  this.router.navigate([""]);
+}
+
 }
 
 
