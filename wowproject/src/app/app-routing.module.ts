b64c7daa7daf3a76c89f7f713d88d020b0eff27b
M1estere <1234515@MAIL.RU>
2019-12-10 15:22:41 +0300

    Самый главный файл за урок, короче

--- wowproject/src/app/app-routing.module.ts
+++ wowproject/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './Lesson 10.12/home/home.component';
 import { AboutComponent } from './Lesson 10.12/about/about.component';
 import { NotFoundComponent } from './Lesson 10.12/not-found/not-found.component';
 import { ItemComponent } from './Lesson 10.12/item/item.component';
+import { ItemStatComponent } from './Lesson 10.12/item/item-stat/item-stat.component';
+import { ItemDetailsComponent } from './Lesson 10.12/item/item-details/item-details.component';
 
 
 const routes: Routes = [
   { path: "", component: HomeComponent},
   { path: "about", component: AboutComponent},
-  { path: "item/:id", component:ItemComponent },
+  { path: "item/:id", component:ItemComponent, children: [{path: "details", component: ItemDetailsComponent},
+  {path: "stat", component: ItemStatComponent}
+] },
   {path: "**", component: NotFoundComponent} /* Две звездочки - любой маршрут */
 ];
 
--- wowproject/src/app/app.module.ts
+++ wowproject/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HomeComponent } from './Lesson 10.12/home/home.component';
 import { AboutComponent } from './Lesson 10.12/about/about.component';
 import { NotFoundComponent } from './Lesson 10.12/not-found/not-found.component';
 import { ItemComponent } from './Lesson 10.12/item/item.component';
+import { ItemDetailsComponent } from './Lesson 10.12/item/item-details/item-details.component';
+import { ItemStatComponent } from './Lesson 10.12/item/item-stat/item-stat.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { ItemComponent } from './Lesson 10.12/item/item.component';
     HomeComponent,
     AboutComponent,
     NotFoundComponent,
-    ItemComponent
+    ItemComponent,
+    ItemDetailsComponent,
+    ItemStatComponent
   ],
   imports: [
     FormsModule,
